76db00fcdfbd2c830a854d9373872d65480f3c2a
logesh <104132894+logeshjeevaraj@users.noreply.github.com>
2022-07-02 13:01:01 +0530

    Add files via upload
    Day 3 task

---
+++ day 3.js
@@ -0,0 +1,20 @@
+var sample = [1 ,2 ,3 ,4 ,5]
+for(i=0;i<=sample.length;i++)
+// console.log(sample)
+
+var sampleJSON= JSON.stringify(sample)
+console.log(sampleJSON) // this is for for loop
+
+let person ={
+    name: 'loki';
+    age: 26;
+    e-mail: 'loki@gmail.com';
+}for(let Content in person )
+    console.log('content is'+ key + 'details is ' + person[key]);
+
+    let arr=['apple','mango','orange','grapes']
+    for (let ele of arr){
+        console.log(ele)
+    }; 
+    //  this is for of loop
+
